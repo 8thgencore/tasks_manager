752a1e71d34fd68bc5bb602192538828a1b23b2e
axelmaker <axelmaker@ya.ru>
2022-05-18 22:45:33 +0300

    Build React App

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import environ
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     "djoser",
     "users",
     "tasks",
+    "frontend",
 ]
 
 GOOGLE_RECAPTCHA_SECRET = env("GOOGLE_RECAPTCHA_SECRET", default="")
@@ -149,7 +151,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "frontend/static")
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls.conf import include
 
+from frontend import views as frontend_views
 from users.urls import urlpatterns as users_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", frontend_views.index),
     path("", include("tasks.urls")),
 ]
 urlpatterns += users_urlpatterns
+urlpatterns += [re_path(r"^.*", frontend_views.index)]

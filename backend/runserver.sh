5818428d0e8e6d7531690e5f25ded5203d2e9194
8thgencore <axelmaker@ya.ru>
2022-05-23 14:02:13 +0000

    Add captain

--- backend/runserver.sh
+++ backend/runserver.sh
@@ -2,4 +2,4 @@
 
 python backend/manage.py collectstatic --no-input
 python backend/manage.py migrate
-gunicorn backend.app.wsgi:application --bind 0.0.0.0:8000
+gunicorn backend.app.wsgi:application --bind 0.0.0.0:8055
